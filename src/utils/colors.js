0c4aba399c9ad8093d6344b4f8897220f9eec917
Red833 <harshavardhan833202@gmail.com>
2026-05-18 17:32:52 +0530

    feat: add Deployment type and rename Review to Reviews in task dropdown

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,22 +1,24 @@
 export const TYPE_COLORS = {
-  task:     '#185FA5',
-  issue:    '#E24B4A',
-  analysis: '#534AB7',
-  meeting:  '#BA7517',
-  review:   '#1D9E75',
-  break:    '#888780',
-  other:    '#5F5E5A',
+  task:       '#185FA5',
+  issue:      '#E24B4A',
+  analysis:   '#534AB7',
+  meeting:    '#BA7517',
+  review:     '#1D9E75',
+  deployment: '#D4611A',
+  break:      '#888780',
+  other:      '#5F5E5A',
 };
 
 export const TYPE_LABELS = {
-  task:     'Task',
-  issue:    'Issue',
-  analysis: 'Analysis',
-  meeting:  'Meeting',
-  review:   'Review',
-  break:    'Break',
-  other:    'Other',
+  task:       'Task',
+  issue:      'Issue',
+  analysis:   'Analysis',
+  meeting:    'Meeting',
+  review:     'Reviews',
+  deployment: 'Deployment',
+  break:      'Break',
+  other:      'Other',
 };
 
 /** Ordered list of all types */
-export const ALL_TYPES = ['task', 'issue', 'analysis', 'meeting', 'review', 'break', 'other'];
+export const ALL_TYPES = ['task', 'issue', 'analysis', 'meeting', 'review', 'deployment', 'break', 'other'];
